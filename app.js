9b05068042f018b4ecdc7a1d817f5d97296b4e31
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-09-28 18:13:03 +0200

    added comments

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
-app.use(express.json());
+
+app.use(express.json());  // This middleware parses incoming requests with JSON payloads and makes the parsed data available on the req.body object.
+
 const port = 8080 || process.env.PORT;
 const travel = require("./travel-list.js")
 
+
+// 1. GET / api/countries
+// This should respond with a list of countries in your list. You should start off with 5 countries.
 
 app.get("/", (req, res) => {
     res.send("Travel Wishlist")
@@ -14,8 +19,12 @@ app.get("/countries", ((req, res) => {
     res.json(travel)
 }))
 
+
+// 2. POST /api/countries
+// This route should accept JSON data and add the new country received to the list of countries (eg: add an object inside the countries array).
+
 app.post("/countries", (req,res) => {
-    const country = req.body
+    const country = req.body  // POSTMAN
     console.log(country)
 
     travel.push(country)
@@ -23,6 +32,9 @@ app.post("/countries", (req,res) => {
     res.json(country)
 })
 
+
+// 3. GET /api/countries/:code
+// This route should return a single country, based on the code provided. You should accept both an alpha 2 or an alpha 3 code. 
 
 app.get("/countries/:code", (req, res) => {
 const singleCountry = req.params.code
@@ -36,12 +48,20 @@ if(!countryCode){
   res.json(countryCode);
 });
 
+
+// 4. PUT /api/countries/:code
+// This route should accept edits to an existing country in the list (eg: edit an object inside the countries array).
+
 app.put("/countries/:code", (req, res) => {
     const country = req.body
     console.log(country)
-    res.json(country)
+    res.json(country, `PUT request`)
 })
 
+
+
+// 5. DELETE /api/countries/:code
+// This route should allow you to delete a specific country from the list (eg: remove an object from the array)
 
 app.delete("/countries/:code", (req, res) => {
     const singleCountry = req.params.code
@@ -55,6 +75,7 @@ app.delete("/countries/:code", (req, res) => {
         return res.status(404).send("Country not found")
     }   
 })
+
 
 
 app.listen(port, () => {
